28e4e156cfec2fabd3e6e375b6f3954eef1255df
christian <jaekl@gmx.at>
2022-01-09 14:36:37 +0100

    ocr test

---
+++ src/app/Language.ts
@@ -0,0 +1,5 @@
+
+export interface ILanguageSwitch {
+  name: string;
+  value: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js';
+import {ILanguageSwitch} from "./Language";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'kleine-zeitung-kundennummer';
+  title = 'OCR Angular Image Tesseract Test';
+  number = '';
+  responseMessage = '';
+  img: any;
+  isLoading = false;
+  language = 'eng';
+  languageSwitch: ILanguageSwitch[] = [
+    {name: 'English: ', value: 'eng'},
+    {name: 'Deutsch: ', value: 'deu'}
+  ]
+
+  async addedFile(event: Event) {
+    // @ts-ignore
+    this.img = event?.target?.files[0];
+  }
+
+  async doOCR() {
+    this.isLoading = true;
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage(this.language);
+    await worker.initialize(this.language);
+    const { data: { text } } = await worker.recognize(this.img);
+    this.responseMessage = text;
+    console.log(text);
+    await worker.terminate().then(() => {
+      this.isLoading = false;
+    });
+  }
 }
